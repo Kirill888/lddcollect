0e58dd2309f3afb90f309508e5e81c4f9ab53c51
Kirill Kouzoubov <kirill888@gmail.com>
2020-03-21 20:58:03 +1100

    New features for CLI app
    - Files and packages are now sorted on output
    - Default is now `--no-dpkg`
    - Add `--json` output option
    - Changed separator line to be 3 dots "..."

--- lddcollect/__main__.py
+++ lddcollect/__main__.py
@@ -1,21 +1,30 @@
+import sys
+from json import (dump as json_dump)
 import click
 from . import process_elf
 
 
 @click.command(name='lddcollect')
-@click.option('--dpkg/--no-dpkg', default=True, help="Lookup dpkg libs or not")
+@click.option('--dpkg/--no-dpkg', default=False, help="Lookup dpkg libs or not, default: no")
+@click.option('--json', is_flag=True, help="Output in json format")
 @click.option('--verbose', is_flag=True, help="Print some info to stderr")
 @click.argument('libs',
                 nargs=-1,
                 type=click.Path(exists=True, dir_okay=False, file_okay=True))
-def main(libs, dpkg=False, verbose=False):
-    """Find all other libraries and debian dependencies this executable or library
-    requires to run.
+def main(libs, dpkg=False, json=False, verbose=False):
+    """Find all other libraries and optionally Debian dependencies listed
+    applications/libraries require to run.
 
-    Prints:
-    1. Non-dpkg managed files, one per line
-    2. Separator line: --------------------------------
-    3. Package names, one per line
+    Prints libraries (including symlinks) that are referenced by input files, one
+    file per line.
+
+    When --dpkg option is supplied, print:
+
+    \b
+      1. Non-dpkg managed files, one per line
+      2. Separator line: ...
+      3. Package names, one per line
+
     """
 
     debs = set()
@@ -26,13 +35,22 @@ def main(libs, dpkg=False, verbose=False):
         debs.update(_debs)
         files.update(_files)
 
-    for file in files:
-        print(file)
+    files = sorted(files)
+    debs = sorted(debs) if dpkg else None
 
-    if dpkg:
-        print("------------------------------------------")
-        for deb in debs:
-            print(deb)
+    if json:
+        out = {'files': files}
+        if debs is not None:
+            out['debs'] = debs
+        json_dump(out, sys.stdout, indent=2)
+    else:
+        for file in files:
+            print(file)
+
+        if debs is not None:
+            print("...")
+            for deb in debs:
+                print(deb)
 
 
 main()
